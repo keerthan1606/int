db90a416a4632a6a119250489b302c749d7cdbf1
keerthan1606 <spacekeerthan1116@gmail.com>
2026-08-03 18:42:25 +0530

    m

--- keerthan/src/App.jsx
+++ keerthan/src/App.jsx
@@ -2,13 +2,13 @@ import React from "react";
 import "./App.css";
 
 // Import your components
-import Navbar from "./components/navbar/navbar";
-import Hero from "./components/hero/hero";
-import About from "./components/about/about";
-import Skill from "./components/skills/skill";
-import Project from "./components/projects/project";
-import Contact from "keerthan/src/components/Contact/contact.jsx";
-import Footer from "./components/footer/footer";
+import navbar from "./components/navbar/navbar";
+import hero from "./components/hero/hero";
+import about from "./components/about/about";
+import skill from "./components/skills/skill";
+import project from "./components/projects/project";
+import contact from "./components/Contact/contact";
+import footer from "./components/footer/footer";
 
 function App() {
   return (

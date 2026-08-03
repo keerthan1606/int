3bcf4e411544aa8a0206787f5d6b909e987da150
keerthan1606 <spacekeerthan1116@gmail.com>
2026-08-03 18:11:47 +0530

    HELLO

--- keerthan/src/App.jsx
+++ keerthan/src/App.jsx
@@ -7,7 +7,7 @@ import Hero from "./components/hero/hero";
 import About from "./components/about/about";
 import Skill from "./components/skills/skill";
 import Project from "./components/projects/project";
-import Contact from "./components/Contact/contact";
+import Contact from "./components/Contact/Contact.jsx";
 import Footer from "./components/footer/footer";
 
 function App() {
